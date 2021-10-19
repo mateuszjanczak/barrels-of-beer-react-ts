b34d5a0c533c1a196694765475b56f799448e0af
Mateusz Janczak <mateuszjanczak.mj@gmail.com>
2021-10-19 19:14:31 +0200

    BoBc-9 Set up navbar (#11)
    * BoBc-9 Add Bootstrap and Material UI
    
    * BoBc-9 Set up navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,18 +2,61 @@ import React from "react";
 import {NavLink} from "react-router-dom";
 import {routes} from "../routes/Routes";
 
-const Navbar = () => (
-    <div>
-        <ul>
-            <li>
-                <NavLink to={routes.taps}>Taps</NavLink>
-            </li>
+class Navbar extends React.Component {
 
-            <li>
-                <NavLink to={routes.events}>Events</NavLink>
-            </li>
-        </ul>
-    </div>
-);
+    state = {
+        visible: false
+    }
+
+    toggleNav = () => {
+        this.setState({
+            visible: !this.state.visible
+        })
+    }
+
+    showNav = () => {
+        this.setState({
+            visible: true
+        })
+    }
+
+    hideNav = () => {
+        this.setState({
+            visible: false
+        })
+    }
+
+    render() {
+        return (
+            <div className="navbar navbar-expand-lg navbar-light bg-light">
+                <div className="container-fluid">
+                    <NavLink to={routes.homepage} className="navbar-brand" href="#">Browar Trzy Korony</NavLink>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
+                            data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                            aria-expanded="false" aria-label="Toggle navigation" onClick={this.toggleNav}>
+                        <span className="navbar-toggler-icon"/>
+                    </button>
+                    <div className={`collapse navbar-collapse ${this.state.visible ? "show" : ""} flex-grow-1`} id="navbarSupportedContent">
+                        <ul className="navbar-nav me-auto">
+                            <li className="nav-item">
+                                <NavLink to={routes.taps} className="nav-link active" onClick={this.hideNav}>Taps</NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink to={routes.events} className="nav-link active" onClick={this.hideNav}>Events</NavLink>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className={`collapse navbar-collapse ${this.state.visible ? "show" : ""} flex-grow-0`} id="navbarSupportedContent">
+                        <ul className="navbar-nav me-auto">
+                            <li className="nav-item">
+                                <NavLink to={routes.taps} className="nav-link active" onClick={this.hideNav}>Admin panel</NavLink>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
 
 export default Navbar;

3aab7ddc4c7b987c2692a0861e4186d8069cb88b
mateuszjanczak <mateuszjanczak.mj@gmail.com>
2021-10-19 20:07:46 +0200

    BoBc-8 Create events view

---
+++ src/components/ActionEvent.tsx
@@ -0,0 +1,73 @@
+import React from "react";
+import styled from "styled-components";
+import {IActionEvent} from "../model/response/IActionEvent";
+import {Pagination} from "@material-ui/lab";
+
+type Props = {
+    actionEvents: IActionEvent
+    handleChangePage: (event, page: number) => void
+}
+
+class ActionEvent extends React.Component<Props> {
+
+    render() {
+        const {actionEvents, handleChangePage} = this.props;
+
+        return (
+            <>
+                <Heading>Action</Heading>
+                {actionEvents.content.length === 0 && <p className="text-center">No data</p>}
+
+                <Container className="table-responsive bg-light">
+                    <table className="table mb-0">
+                        <thead>
+                        {actionEvents.content.length > 0 &&
+                        <tr>
+                            <th scope="col">Action ID</th>
+                            <th scope="col">Tap</th>
+                            <th scope="col">Barrel content</th>
+                            <th scope="col">Current level</th>
+                            <th scope="col">Total usage</th>
+                            <th scope="col">Single usage</th>
+                            <th scope="col">Date</th>
+                            <th scope="col">Action Type</th>
+                        </tr>}
+                        </thead>
+                        <tbody>
+                        {actionEvents.content.map(({id, tapId, barrelContent, currentLevel, totalUsage, singleUsage, date, logType}) => (
+                            <tr key={id}>
+                                <th scope="row">{id}</th>
+                                <td>{tapId}</td>
+                                <td>{barrelContent}</td>
+                                <td>{currentLevel/1000} L</td>
+                                <td>{totalUsage/1000} L</td>
+                                <td>{singleUsage/1000} L</td>
+                                <td>{date}</td>
+                                <td>{logType}</td>
+                            </tr>
+                        ))}
+                        </tbody>
+                    </table>
+                    {actionEvents.content.length > 0 && <PaginationContainer>
+                        <Pagination count={actionEvents.totalPages} color="primary" onChange={handleChangePage}/>
+                    </PaginationContainer>}
+                </Container>
+            </>
+        );
+    }
+}
+
+const Heading = styled.h3``;
+
+const Container = styled.div`
+  margin-bottom: 2rem;
+`;
+
+const PaginationContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 1rem;
+`;
+
+export default ActionEvent;
--- src/views/EventsView.tsx
+++ src/views/EventsView.tsx
@@ -1,7 +1,63 @@
 import React from "react";
+import styled from "styled-components";
+import EventsService from "../service/EventService";
+import ActionEvent from "../components/ActionEvent";
+import TemperatureEvent from "../components/TemperatureEvent";
 
-const EventsView = () => (
-    <p>EventsView.tsx</p>
-);
+class EventsView extends React.Component {
+
+    state = {
+        actionEvents: {
+            content: [],
+            totalPages: 0
+        },
+        temperatureEvents: {
+            content: [],
+            totalPages: 0
+        }
+    }
+
+    componentDidMount() {
+        this.fetchEvents()
+    }
+
+    fetchEvents = () => {
+        this.fetchActionEvents(0)
+        this.fetchTemperatureEvents(0)
+    }
+
+    fetchActionEvents = (page: number) => {
+        EventsService.getActionEvents(page).then(actionEvents => this.setState({actionEvents}))
+    }
+
+    fetchTemperatureEvents = (page: number) => {
+        EventsService.getTemperatureEvents(page).then(temperatureEvents => this.setState({temperatureEvents}))
+    }
+
+    handleChangePageActionEvents = (event, value) => {
+        this.fetchActionEvents(value - 1);
+    }
+
+    handleChangePageTemperatureEvents = (event, value) => {
+        this.fetchTemperatureEvents(value - 1);
+    }
+
+    render() {
+        const {actionEvents, temperatureEvents} = this.state;
+
+        return (
+            <div className="container-fluid">
+                <Heading>Events</Heading>
+                <ActionEvent actionEvents={actionEvents} handleChangePage={this.handleChangePageActionEvents}/>
+                <TemperatureEvent temperatureEvents={temperatureEvents} handleChangePage={this.handleChangePageTemperatureEvents}/>
+            </div>
+        );
+    }
+}
+
+const Heading = styled.h1`
+  text-align: center;
+  margin: 2rem 0;
+`;
 
 export default EventsView;
